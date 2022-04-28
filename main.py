13f711a25928add7e8119b9a7a6a740faa91fa41
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-04-28 19:44:40 +0600

    chore: Обозначил функции

---
+++ main.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+
+def get_old_database(OLD_DATABASE):
+    """Читаем старую базу данных"""
+    pass
+
+
+def get_migrate_data():
+    """Миграция данных"""
+    pass
+
+
+def del_old_database():
+    """Удаление старой базы"""
+    pass
+
